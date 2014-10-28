7fa75f53582821acfa61912e1b4688d3a5bc26a0
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-28 16:27:14 +0530

    more changes.

--- shorten_url.py
+++ shorten_url.py
@@ -15,9 +15,9 @@ REDIS_IP = '127.0.0.1'
 REDIS_PORT = 6379
 REPLICAS_SIZE = 10  # Number of Replicas
 MIN_EXP_TIME = 24 * 60 * 60     # Expire after 1 day
-REDIRECT_COUNTS_KEY = 'shorturl:resolved'
-HLL_ORIG_URL_KEY = 'hyperloglog:original:url'
-HLL_SHORT_URL_KEY = 'hyperloglog:short:url'
+REDIRECT_COUNTS_KEY = 'url:shorturl:resolved'
+HLL_ORIG_URL_KEY = 'url:hyperloglog:original'
+SET_SHORT_URL_KEY = 'url:short:set'
 
 #  TODO: try using zmq  based ioloop instead might be more useful
 #  TODO: add that ConsistentHashRing setup to enable redis cluster
@@ -51,28 +51,37 @@ class UrlShortener(object):
     def __init__(self):
         self.redis = redis.Redis(host=REDIS_IP, port=REDIS_PORT)
 
+    def get_stats(self):
+        stats = {"urls":self.redis.pfcount(HLL_ORIG_URL_KEY),
+                 "redirects": self.redis.get(REDIRECT_COUNTS_KEY)
+                 }
+        return stats
+
     def shorten_url(self, url):
         orig_url_not_exists = self.redis.pfadd(HLL_ORIG_URL_KEY, url)
-        short_url_not_exists = self.redis.pfadd(HLL_SHORT_URL_KEY, url)
-        if orig_url_not_exists and short_url_not_exists:
+        # Check if the given url is a shortened url. stop malicious programs from inducing a redirect loop
+        short_url_not_exists = self.redis.sadd(SET_SHORT_URL_KEY, url)
+
+        if not short_url_not_exists:
+            logging.warn("#urlshortener: short_url provided as input for shortening")
+            return None
+        # If execution reaches here input is not a shortened url, remove it from storage.
+        self.redis.srem(SET_SHORT_URL_KEY, url)
+        if orig_url_not_exists:
             short_url = "".join([random.choice(self.URL_ALLOWED_CHARS) for i in range(5)])
             if not self.redis.get(short_url):
                 self.redis.setex(short_url, url, MIN_EXP_TIME)
                 self.redis.setex(url, short_url, MIN_EXP_TIME)
-                self.redis.pfadd(HLL_SHORT_URL_KEY, short_url)
+                self.redis.sadd(SET_SHORT_URL_KEY, short_url)
             else:
                 # Since collisions are possible, this means there was a
                 # collision
                 logging.warn("#urlshortener: Collision Orig Url: %s, generated short url: %s" %(url, short_url))
                 self.shorten_url(url)
-        elif not short_url_not_exists and orig_url_not_exists:
+        else:
             # Original url already shortenede, just return th
             short_url = self.redis.get(url)
-        else:
-            logging.warn("#urlshortener: short_url provided as input for shortening")
-            short_url  = "Piss off, don't play"
-        assert short_url
-        return str(short_url)
+        return short_url
 
     def retrieve_orig_url(self, short_url):
         return str(self.redis.get(short_url))
@@ -81,6 +90,7 @@ class ShortUrlHandler(RequestHandler):
     def get(self, args):
         logging.info(args)
         if args:
+            print args
             data = {"short_url": args}
             assert url_shortener.redis.get(args)
             self.request.query_arguments.update(data)
@@ -101,8 +111,18 @@ class ShortenUrlHandler(RequestHandler):
     def post(self):
         orig_url = self.get_argument('orig_url')
         logging.info('# Received Original url: %s' % orig_url)
-        short_url = '<a href=' + '/'.join([self.request.headers.get('Origin'), url_shortener.shorten_url(orig_url)]) + '>Click Here</a>'
-        self.finish(json.dumps({'url': short_url}))
+
+        import pdb; pdb.set_trace()
+        short_url = url_shortener.shorten_url(orig_url)
+        if short_url:
+            linkified_short_url = '<a href=' + '/'.join([self.request.headers.get('Origin'), 'url', short_url]) + '>Click Here</a>'
+            self.finish(json.dumps({'url': linkified_short_url}))
+        else:
+            self.redirect("/url/")
+
+class StatsHandler(RequestHandler):
+    def get(self):
+        self.finish(json.dumps(url_shortener.get_stats()))
 
 class Application(Application):
     #  """
@@ -114,6 +134,7 @@ class Application(Application):
     def __init__(self):
         handlers = [
                 (r'/url/shorten', ShortenUrlHandler),
+                (r'/url/stats',StatsHandler),
                 (r'/url/(.*)', ShortUrlHandler),
                 ]
         settings = dict(
@@ -130,6 +151,7 @@ def main():
     app.listen(options.port, xheaders=True)
     loop = tornado.ioloop.IOLoop.instance()
     loop.start()
+
 if __name__ == '__main__':
     url_shortener = UrlShortener()
     main()
