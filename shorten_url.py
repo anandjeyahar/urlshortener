8bd3ce6ef7eefac497e8728f2de35af3fc75c297
Software Mechanic <softwaremechanic32@gmail.com>
2014-10-29 14:59:13 +0530

    oum

--- shorten_url.py
+++ shorten_url.py
@@ -111,7 +111,7 @@ class ShortenUrlHandler(RequestHandler):
         logging.info('# Received Original url: %s' % orig_url)
         short_url = url_shortener.shorten_url(orig_url)
         if short_url:
-            linkified_short_url = '<a href=' + '/'.join([self.request.headers.get('Origin'), 'url', urllib.urlencode(short_url)]) + '>Click Here</a>'
+            linkified_short_url = '<a href=' + '/'.join([self.request.headers.get('Origin'), 'url', urllib.quote_plus(short_url)]) + '>Click Here</a>'
             self.finish(json.dumps({'url': linkified_short_url}, ensure_ascii=False).encode('utf-8'))
 
 class StatsHandler(RequestHandler):
