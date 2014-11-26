31b9d34969c48befe1bf5d64836e8f7e1a0ad935
Software Mechanic <softwaremechanic32@gmail.com>
2014-11-27 01:26:58 +0530

    morke change

--- shorten_url.py
+++ shorten_url.py
@@ -154,7 +154,7 @@ def main():
     tornado.options.parse_command_line()
     app = Application()
     httpServer = tornado.httpserver.HTTPServer(app)
-    httpServer.listen(address='0.0.0.0', port=options.port,)
+    httpServer.listen(address='0.0.0.0', port=os.getenv('PORT'))
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == '__main__':
